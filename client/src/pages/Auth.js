126b4bf277155b884369a28e1a93175713522ffb
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-05 00:05:57 +0300

    Logic to catch errors in the register form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -8,10 +8,13 @@ export const Auth = () => {
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const navigate = useNavigate
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
 
+
+// Connect the register form to the register API
   const handleSubmit = async (event) => {
-    event.prevenDefault()
+    event.preventDefault()
 
     try {
       await axios.post('http://localhost:5000/api/register', {
@@ -19,10 +22,14 @@ export const Auth = () => {
         email,
         password
       })
-      alert('Registration Complete')
-      navigate('/')
+      // alert('Registration Complete')
+      navigate("/")
     } catch (error) {
-      console.error(error)
+      if(error.response && error.response.status === 400) {
+        setError(error.response.data.message)
+      } else {
+        console.error(error)
+      }
     }
   }
 
@@ -31,13 +38,17 @@ export const Auth = () => {
     <div className="auth-container">
     <form onSubmit={handleSubmit}>
       <h2>Register</h2>
+      {error && <div className="error-message">{error}</div>}      
+
       <div className="form-group">
         <label htmlFor="username">Username </label>
         <input 
           type="text"
           id="username" 
           value={username}
-          onChange={(event) => setUsername(event.target.value)} />
+          onChange={(event) => setUsername(event.target.value)} 
+          required
+          />
       </div>
       <div className="form-group">
         <label htmlFor="email">Email </label>
@@ -45,7 +56,9 @@ export const Auth = () => {
           type="email" 
           id="email" 
           value={email}
-          onChange={(event) => setEmail(event.target.value)} />
+          onChange={(event) => setEmail(event.target.value)} 
+          // required
+          />
       </div>
       <div className="form-group">
         <label htmlFor="password">Password </label>
@@ -53,14 +66,16 @@ export const Auth = () => {
           type="password" 
           id="password" 
           value={password}
-          onChange={(event) => setPassword(event.target.value)} />
+          onChange={(event) => setPassword(event.target.value)} 
+          // required
+          />
       </div>
       <button type="submit">Register</button>
 
       
       <p>
           Already have an account? <Link to="/login">Login</Link>
-        </p>      
+        </p>
     </form>
   </div>
   ) 
