5a24c859eb21ff67c4d7524a8a34c20ecb363399
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-08 13:04:27 +0300

    added logout functionality to allow user to logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,32 @@
 import {Link} from 'react-router-dom'
+import { FaSignOutAlt } from 'react-icons/fa'
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom' 
 
 
 export const Navbar = () => {
+  // Logout functionality
+  const [cookies, setCookies] = useCookies(["access_token"])
+  const navigate = useNavigate()
+
+  const logout = () => {
+    setCookies("access_token", "")
+    window.localStorage.removeItem("userID")
+    navigate('/register')
+  }
   return (
     <div className='navbar'>
       <Link to='/'> Home</Link>
       <Link to='/createProject'>Create Project</Link>
       <Link to='/savedProject'> Saved Projects</Link>
+      {!cookies.access_token ? ( 
       <Link to='/register'>Login/Register</Link>
+      ) : (
+        <button onClick={logout}>
+          <FaSignOutAlt />Logout
+          </button>
+      )}
+     
     </div>
   )
 }
