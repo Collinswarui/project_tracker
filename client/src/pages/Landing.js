c79996b1804876aa0f46efac5d896e2c2e240cf8
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-08 14:38:59 +0300

    adding content to the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,25 @@
-export const Landing = () => {
+ export const Landing = () => {
     return (
-        <div>Landing Page</div>
-    )
-}
+      <div className="landing">
+        <h1>Welcome to Project Tracker</h1>
+        <p>Track and manage your projects with ease using our project tracking system. Whether you're working on personal projects, collaborating with a team, or managing a business, our tool can help you stay organized and on top of your project's progress.</p>
+  
+        <h2>Key Features:</h2>
+        <ul>
+          <li>Project Creation: Easily create new projects and define project details.</li>
+          <li>Task Management: Break down your projects into tasks and set deadlines.</li>
+          <li>Progress Tracking: Monitor the progress of your projects and tasks.</li>
+          <li>Collaboration: Collaborate with team members and assign tasks.</li>
+          <li>Deadline Reminders: Get timely reminders for upcoming deadlines.</li>
+        </ul>
+  
+        <h2>Get Started:</h2>
+        <p>Ready to take control of your projects? Sign up or log in to get started with Project Tracker. With our user-friendly interface, you can start managing your projects more efficiently today.</p>
+  
+        <div className="cta-buttons">
+          <button>Sign Up</button>
+          <button>Log In</button>
+        </div>
+      </div>
+    );
+  }
